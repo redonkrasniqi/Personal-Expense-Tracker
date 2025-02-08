dbc73cf404c2f6da6bb7fd95b9c194f77f6ae03c
Redon Krasniqi <krasniqiredon2004@gmail.com>
2025-02-08 22:59:07 +0100

    cors setup and linking api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
+import cors from 'cors';
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+};
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
