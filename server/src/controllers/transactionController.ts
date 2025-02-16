b0e808a9c6b35a9ac7e92af33787fc66d8e78ad5
redon <redon@91.life>
2025-02-16 01:39:46 +0100

    delete a transaction endpoint

--- server/src/controllers/transactionController.ts
+++ server/src/controllers/transactionController.ts
@@ -15,3 +15,19 @@ export const createTransaction = async (req: Request, res: Response): Promise<vo
         res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+export const deleteTransaction = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.body;
+
+        if (!id) {
+            res.status(400).json({ message: 'Transaction ID is required for deleting transaction' });
+        }
+
+        console.log("the request body: ", req.body)
+        return
+    } catch (error) {
+        console.error('Error deleting a transaction:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
